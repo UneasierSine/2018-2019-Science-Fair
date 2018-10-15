856b7a208091a9ebb63b7dd0c836a95103641dc9
UneasierSine <32441247+UneasierSine@users.noreply.github.com>
2018-10-15 10:32:14 -0400

    work on the gpu part of the matrix math implementation

---
+++ ConvNet_2018-19/ConvNet_2018-19/MatrixMathGpu.cu
@@ -0,0 +1,83 @@
+#include <iostream>
+#include "MatrixMath.h"
+#include "MatrixMath.cuh"
+
+__global__ void add(double *a, double *b)
+{
+
+}
+
+__global__ void sub(double *a, double *b)
+{
+
+}
+
+__global__ void mul(double *a, double *b)
+{
+
+}
+
+__global__ void div(double *a, double *b)
+{
+
+}
+
+__global__ void pow(double *a, double *b)
+{
+
+}
+
+__global__ void rad(double *a, double *b)
+{
+
+}
+
+__global__ void sumTerms(double *a)
+{
+
+}
+
+__global__ void dotProduct(double *a, double *b)
+{
+
+}
+
+vector<double> addMatTermsGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
+
+vector<double> subMatTermsGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
+
+vector<double> mulMatTermsGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
+
+vector<double> divMatTermsGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
+
+vector<double> powMatTermsGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
+
+vector<double> radMatTermsGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
+
+double sumTermsGpu(vector<double> vector)
+{
+
+}
+
+double dotProductGpu(vector<double> vec1, vector<double> vec2)
+{
+
+}
